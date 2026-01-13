562f679e871706c2b8d71dc63394fdf0bde97dae
Justin <contact@justinpitts.dev>
2026-01-13 15:54:11 -0500

    Create FetchAndSaveGithubUserJob

---
+++ app/jobs/fetch_and_save_github_user_job.rb
@@ -0,0 +1,18 @@
+class FetchAndSaveGithubUserJob < ApplicationJob
+  # Retry on transient errors with exponential backoff
+  retry_on Github::Client::ServerError, wait: :exponentially_longer, attempts: 5
+
+  # Retry on rate limits with long delay (GitHub rate limit window is ~1 hour)
+  retry_on Github::Client::RateLimitError, wait: 1.hour, attempts: 3
+
+  # Don't retry on permanent errors (404 = user deleted, 403 = access denied)
+  discard_on Github::Client::ClientError
+
+  # Retry on database errors
+  retry_on ActiveRecord::Deadlocked, wait: 5.seconds, attempts: 3
+  retry_on ActiveRecord::ConnectionNotEstablished, wait: 5.seconds, attempts: 3
+
+  def perform(username)
+    GithubUserFetcher.new.call(username: username)
+  end
+end
